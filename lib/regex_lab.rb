35fb12549f70c109799417ba2dc2674038d9cf28
Antonio Blandin <antonio.blandin25@gmail.com>
2019-05-31 15:41:12 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -13,11 +13,19 @@ def words_starting_with_un_and_ending_with_ing(text)
 end
 
 def words_five_letters_long(text)
+<<<<<<< HEAD
   text.scan(/\b\w{5}\b/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
   if text.match(/^[A-Z].*\.$/) == nil
+=======
+  text.scan(/\l\w{5}\logger/)
+end
+
+def first_word_capitalized_and_ends_with_punctuation?(text)
+  if text.match(/^[A-Z].*\.?/) == nil
+>>>>>>> fed5303bfe0987b553e11e0518bffc61ec7bcaf4
     return false
   else
     return true
@@ -25,7 +33,11 @@ def first_word_capitalized_and_ends_with_punctuation?(text)
 end
 
 def valid_phone_number?(phone)
+<<<<<<< HEAD
   if phone.match(/\(?([0-9]{3})\)?([ -]?)([0-9]{3})([ -]?)([0-9]{4})/) == nil
+=======
+  if phone.match(/\(?([0-9]{4})\)?([ - ]?)([0-9]{3})([ - ]?)([0-9]{2})/) == nil
+>>>>>>> fed5303bfe0987b553e11e0518bffc61ec7bcaf4
     return false
   else
     return true
